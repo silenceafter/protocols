4cc6bf325e0cda3ec544c126bd1206b7124a7fc6
silenceafter <a59-info@yandex.ru>
2022-12-20 19:01:00 +0300

    LibraryService

---
+++ homework1/LibraryService/LibraryService/Services/Interfaces/ILibraryRepository.cs
@@ -0,0 +1,16 @@
+﻿using LibraryService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryService.Services.Interfaces
+{
+    internal interface ILibraryRepository
+    {
+        IList<Book> GetByAuthor(string authorName);
+        IList<Book> GetByCategory(string category);
+        IList<Book> GetByTitle(string title);                
+    }
+}
---
+++ homework1/LibraryService/LibraryService/Services/LibraryRepository.cs
@@ -0,0 +1,56 @@
+﻿using LibraryService.Models;
+using LibraryService.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryService.Services
+{
+    public class LibraryRepository : ILibraryRepository
+    {       
+        private readonly ILibraryDatabaseContext _context;
+        
+        public LibraryRepository(ILibraryDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> GetByAuthor(string authorName)
+        {
+            try
+            {
+                return _context.Books.Where(book => book.Authors.Where(author => author.Name.ToLower().Contains(authorName.ToLower())).Count() > 0).ToList();
+            }
+            catch (Exception ex)
+            {
+                return new List<Book>();
+            }
+        }
+
+        public IList<Book> GetByCategory(string category)
+        {
+            try
+            {
+                return _context.Books.Where(book => book.Category.ToLower().Contains(category.ToLower())).ToList();
+            }
+            catch (Exception ex)
+            {
+                return new List<Book>();
+            }
+        }
+        
+        public IList<Book> GetByTitle(string title)
+        {
+            try
+            {
+                return _context.Books.Where(book => book.Title.ToLower().Contains(title.ToLower())).ToList();
+            }
+            catch (Exception ex)
+            {
+                return new List<Book>();
+            }
+        }
+    }
+}
